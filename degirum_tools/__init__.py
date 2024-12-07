bb2f5ba4e13fa9bb9874d3e7301f9ee88184ed07
Vlad Klimov <vladk@degirum.com>
2024-12-06 18:54:07 -0800

    logger support

--- degirum_tools/__init__.py
+++ degirum_tools/__init__.py
@@ -5,6 +5,49 @@
 # All rights reserved
 #
 
+import logging
+from typing import Optional
+
+
+def logger_get():
+    """
+    Get the package logger.
+
+    Returns:
+        Returns the package logger.
+    """
+    return logging.getLogger(__name__)
+
+
+def logger_add_handler(
+    handler: Optional[logging.Handler] = None,
+    format: str = "",
+    level: int = logging.DEBUG,
+) -> logging.Handler:
+    """
+    Add a handler to the package logger.
+
+    Args:
+        handler: Handler to add to the logger. If None, a new StreamHandler to console is added.
+        format: Format string for handler formatter. Defaults to "%(asctime)s [%(levelname)s][%(threadName)s] %(message)s".
+        level: Logging level as defined in logging python package. Defaults to logging.DEBUG.
+
+    Returns:
+        Returns an instance of added handler.
+    """
+    logger = logger_get()
+
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    if not format:
+        format = "%(asctime)s [%(levelname)s][%(threadName)s] %(message)s"
+    handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    return handler
+
+
 # flake8: noqa
 
 from ._version import __version__, __version_info__
