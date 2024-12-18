0ab826eb3d4d1c0658bcd75ea8fb3f2e646b5a6c
lsutardja <lsutardja@gmail.com>
2024-12-18 13:36:37 -0800

    Compound model predict_batch fixes (#72)
    * bugfixes for predict_batch
    
    * check for info type
    
    * bugfix
    
    * FrameInfo propagation fix
    
    * delint

--- degirum_tools/tile_compound_models.py
+++ degirum_tools/tile_compound_models.py
@@ -1,19 +1,17 @@
 import copy
 from typing import List, MutableSequence, Optional, Sequence, Tuple, Union
 
-import cv2
-import numpy as np
+import cv2, numpy as np, degirum as dg
 
-import degirum as dg
-from degirum_tools import (
-    CroppingAndDetectingCompoundModel,
+from .math_support import nms, edge_box_fusion
+from .compound_models import (
+    FrameInfo,
     CropExtentOptions,
     ModelLike,
     NmsOptions,
     MotionDetectOptions,
-    detect_motion,
-)
-from degirum_tools.math_support import nms, edge_box_fusion
+    CroppingAndDetectingCompoundModel,
+    detect_motion)
 
 
 class _TileInfo:
@@ -568,7 +566,7 @@ class LocalGlobalTileModel(TileModel):
 
         else:
             # new frame comes: return combined result of previous frame
-            ret = self._finalize_current_result(result1)
+            ret = self._finalize_current_result(self._current_result1)
             self._current_result = result2
             self._current_result1 = result1
 
@@ -694,6 +692,10 @@ class BoxFusionTileModel(_EdgeMixin, TileModel):
                     use_iou=self._nms_options.use_iou,
                     box_select=self._nms_options.box_select,
                 )
+
+            if isinstance(self._current_result.info, FrameInfo):
+                self._current_result._frame_info = self._current_result.info.original_info
+
             return self._current_result
         return None
 
@@ -770,7 +772,7 @@ class BoxFusionTileModel(_EdgeMixin, TileModel):
 
         else:
             # new frame comes: return combined result of previous frame
-            ret = self._finalize_current_result(result1)
+            ret = self._finalize_current_result(self._current_result1)
             self._current_result = result2
             self._current_result1 = result1
 
@@ -906,7 +908,7 @@ class BoxFusionLocalGlobalTileModel(BoxFusionTileModel):
 
         else:
             # new frame comes: return combined result of previous frame
-            ret = self._finalize_current_result(result1)
+            ret = self._finalize_current_result(self._current_result1)
             self._current_result = result2
             self._current_result1 = result1
 
