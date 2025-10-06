1be092b892c6e5a87d623491ca19f7c24ed4248e
darshil-degirum <c_darshil@degirum.com>
2025-10-06 16:43:36 -0700

    added test_gstreamer script

---
+++ test/test_gstreamer.py
@@ -0,0 +1,7 @@
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst, GLib
+if not Gst.is_initialized():
+    Gst.init(None)
+
+print("GStreamer version:", Gst.version_string())
